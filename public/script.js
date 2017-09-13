3b847159afad47d3ee813ad47df34f7138e265a9
Guillaume Suiffet <suiffet.g@gmail.comm>
2017-09-13 14:58:06 +0200

    Initialisation de Leaflet

--- public/script.js
+++ public/script.js
@@ -20,6 +20,12 @@ $("document").ready(function() {
         //$( "#sortable" ).disableSelection();
     } );
 
+    var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
 /*
   $("li").each(function(i) {
     console.log(i);
