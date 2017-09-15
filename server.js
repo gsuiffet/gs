11fa56f9a7e93b82e37277a96585addd9f617e53
Guillaume Suiffet <suiffet.g@gmail.comm>
2017-09-15 15:30:27 +0200

    Ajout d'un id number aux items dans la bdd

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 var cityList = mongoose.Schema({
+    cityID: Number,
     city: String,
     pictoWeather: String,
     description: String,
@@ -25,38 +26,45 @@ app.get('/', function (req, res) {
 });
 
 app.get('/add', function (req, res) {
-    request("http://api.openweathermap.org/data/2.5/weather?q="+ req.query.city + "+&appid=7a8493c8fbae560841a4fc4b12274eed&lang=fr&units=metric", function(error, response, body) {
-        var body = JSON.parse(body);
+    cityModel.find(function (err, cityList) {
+        request("http://api.openweathermap.org/data/2.5/weather?q="+ req.query.city + "+&appid=7a8493c8fbae560841a4fc4b12274eed&lang=fr&units=metric", function(error, response, body) {
+            var body = JSON.parse(body);
 
-        var citydb = new cityModel ({
-            city: body.name,
-            description: body.weather[0].description,
-            pictoWeather: "http://openweathermap.org/img/w/" + body.weather[0].icon + ".png",
-            maxTemp: body.main.temp_max,
-            minTemp: body.main.temp_min,
-            latitude: body.coord.lat,
-            longitude: body.coord.lon
-        });
-        citydb.save(function (error, cityList) {
-            cityModel.find(function (err, cityList) {
-                res.render("home", {cities: cityList});
+            cityN = cityList.length ++;
+
+            var citydb = new cityModel ({
+                cityID: cityN,
+                city: body.name,
+                description: body.weather[0].description,
+                pictoWeather: "http://openweathermap.org/img/w/" + body.weather[0].icon + ".png",
+                maxTemp: body.main.temp_max,
+                minTemp: body.main.temp_min,
+                latitude: body.coord.lat,
+                longitude: body.coord.lon
+            });
+            //console.log("New ID",citydb);
+
+            citydb.save(function (error, citydb) {
+                cityModel.find(function (err, cityList) {
+                    res.render("home", {cities: cityList});
+                });
             });
         });
     });
 });
 
+// 1 "affecter un odre lorsqu'on ajoute une nouvelle ville" au server
+// 2 demande à bdd la liste des ville
+// 3 récupérer et compter la liste de la db
+
 app.get('/delete', function (req, res) {
-    console.log(req.query);
-    cityModel.remove({city: req.query}, function(error) {
-       // console.log(req.query);
-        console.log();
+    //console.log("INFO 1",req.query.position);
+    cityModel.remove({_id: req.query.position}, function(error) {
         cityModel.find(function (err, cityList) {
             res.render("home", {cities: cityList});
         });
     });
 });
-
-//‘db.test_users.remove({“_id”: { “$oid” : “4d513345cc9374271b02ec6c” }})’
 //cityList.splice(req.query.position, 1)
 
 app.get('/updatePosition', function (req, res) {
@@ -64,21 +72,22 @@ app.get('/updatePosition', function (req, res) {
     var tri = JSON.parse(req.query.tri);
     var cityListTmp = [];
 
+    //cityModel.update({ name: "Doe"}, { age: 32 }, function(error, raw) {
+    //});
+
     //console.log(tri);
     for (var i=0; i<tri.length; i++ ) {
-        console.log("liste 1", cityList[tri[i]]);
+        //console.log("liste 1", cityList[tri[i]]);
         cityListTmp.push(cityList[tri[i]]);
-        //console.log(tri[i]);
-        console.log("liste 2", cityList[tri[i]]);
+        //console.log("liste 2", cityList[tri[i]]);
     }
     cityList = cityListTmp;
     res.send( {data: "hello"});
 });
 
 mongoose.connect('mongodb://guigui:guigui@ds135394.mlab.com:35394/weatherapp' , function(err) {
-    console.log(err);
+    //console.log(err);
 });
-
 
 app.listen(8080, function () {
     console.log("Server listening on port 8080");
